3a52c093f89345a32d1d4f55a8720f4638278a23
ibooga <ibooga@gmail.com>
2025-06-19 14:04:06 -0700

    Configure truly fully associative WT_CLN cache: 2048-way, 1 set

--- core/include/cv32a6_imac_sv32_config_pkg.sv
+++ core/include/cv32a6_imac_sv32_config_pkg.sv
@@ -40,7 +40,7 @@ package cva6_config_pkg;
   localparam CVA6ConfigIcacheSetAssoc = 4;
   localparam CVA6ConfigIcacheLineWidth = 128;
   localparam CVA6ConfigDcacheByteSize = 32768;
-  localparam CVA6ConfigDcacheSetAssoc = 128;
+  localparam CVA6ConfigDcacheSetAssoc = 2048;
   localparam CVA6ConfigDcacheLineWidth = 128;
 
   localparam CVA6ConfigDcacheIdWidth = 3;
